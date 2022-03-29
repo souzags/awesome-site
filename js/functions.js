d78829304b9e03b1bc41618c814d34ec3c57d8c2
Gustavo Sarraf de Souza <gustavosarraf07@gmail.com>
2022-03-29 18:43:57 -0300

    Big update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,11 @@
+var input = document.getElementById("calcInput");
+input.addEventListener("keyup", function(evt) {
+    if (evt.key === "Enter") {
+        evt.preventDefault();
+        document.getElementById("calcButton").click();
+    }
+});
+
 function showSection(evt, sectionName) {
     var content, navlinks;
 
@@ -10,15 +18,46 @@ function showSection(evt, sectionName) {
     }
 
 
-    // Get all navlinks and remove class 'active'
+    // Get all navlinks and remove class 'active'.
     navlinks = document.getElementsByClassName("navlink");
 
     for (let index = 0; index < navlinks.length; index++) {
-        navlinks[index].className = navlinks[index].className.replace("active", "");
+        navlinks[index].className = navlinks[index].className.replace(" active", "");
     }
 
 
-    // Show section that matches 'sectionName' and add 'active' class to the caller button
+    // Show section that matches 'sectionName' and add 'active' class to the caller button.
     document.getElementById(sectionName).style.display = "block";
-    evt.currentTarget.className = "active";
+    evt.currentTarget.className += " active"; // Needs to be added with a space so it doesn't overwrite the navlink class.
+}
+
+// Prime Sieve Function
+function getPrimes() {
+    maxNumber = parseInt(document.getElementById("calcInput").value);
+    output = document.getElementById("calcOutput");
+    n = new Array(maxNumber + 1);
+
+    n.fill(true);
+    output.innerHTML = "";
+
+    n[0] = n[1] = false;
+
+    for (let i = 2; i < n.length - 1; i++) {
+
+        if (n[i]) {
+            for (let j = i + 1; j < n.length; j++) {
+                if (j % i == 0) {
+                    n[j] = false;
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < n.length; i++) {
+        if (n[i]) {
+            output.innerHTML += "<p>" + i + "</p>"
+        }
+    }
+
+    console.log(n);
 }
